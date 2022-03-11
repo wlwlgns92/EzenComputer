e0fed913bc50344a6af6c7327ef6b9c3ffa7450e
yjleekr <yeji2so.kr@gmail.com>
2022-03-11 17:25:44 +0900

    2022.03.11. 제품 레이아웃

---
+++ src/main/java/Ezen/controller/ComponentController.java
@@ -0,0 +1,15 @@
+package Ezen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/component")
+public class ComponentController {
+
+    @GetMapping("/amd")
+    public String amd(){
+        return "cpu/amd";
+    }
+}
