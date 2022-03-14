9f824eb7cf7e58cf09504740709e479e7ba111c3
yjleekr <yeji2so.kr@gmail.com>
2022-03-14 17:59:50 +0900

    2022.03.14. [레이아웃] 장바구니, 결제페이지 완성(id, name, js 이름 수정해야함)

--- src/main/java/Ezen/controller/ComponentController.java
+++ src/main/java/Ezen/controller/ComponentController.java
@@ -10,6 +10,6 @@ public class ComponentController {
 
     @GetMapping("/amd")
     public String amd(){
-        return "product/productcart.html";
+        return "product/productpay.html";
     }
 }
