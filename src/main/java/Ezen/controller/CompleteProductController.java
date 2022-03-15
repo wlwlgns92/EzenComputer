8d87ddeabda98847e64bbfdedef6f21355dfb8c0
wlwlgns92 <wlwlgns92@gmail.com>
2022-03-15 13:59:35 +0900

    노트북 커밋

--- src/main/java/Ezen/controller/CompleteProductController.java
+++ src/main/java/Ezen/controller/CompleteProductController.java
@@ -10,26 +10,36 @@ public class CompleteProductController {
 
     // amd 완제품 페이지 맵핑
     @GetMapping("/amdlist")
-    public String amdlist () {
+    public String amdlist() {
         return "completeproduct/amdlist";
     }
 
     // 게이밍 완제품 페이지 맵핑
     @GetMapping("/gamingpclist")
-    public String gamingpclist () {
+    public String gamingpclist() {
         return "completeproduct/gamingpclist";
     }
 
     // // intel 완제품 페이지 맵핑
     @GetMapping("/intellist")
-    public String intellist () {
+    public String intellist() {
         return "completeproduct/intellist";
     }
-    
+
     // 사무용 완제품 페이지 맵핑
     @GetMapping("/officepclist")
-    public String officepclist () {
+    public String officepclist() {
         return "completeproduct/officepclist";
+    }
+
+    // 상세페이지 [추후에 데이터 같이 넘기는걸로] 
+    @GetMapping("detailpage")
+    public String detailpage() { return "completeproduct/detailpage"; }
+
+    // 전체컴퓨터 페이지
+    @GetMapping("/allcomputer")
+    public String allcomputer() {
+        return "completeproduct/allcomputer";
     }
 
 }
