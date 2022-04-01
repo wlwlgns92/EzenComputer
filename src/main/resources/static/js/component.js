28185db703e046f9f392e952024c16bd66d54f22
wlwlgns92 <wlwlgns92@gmail.com>
2022-04-02 02:44:31 +0900

    이것저것

--- src/main/resources/static/js/component.js
+++ src/main/resources/static/js/component.js
@@ -6,3 +6,16 @@ function CToutput(componentcategoryNo) {
         $("#listbox").replaceWith(fragment);
     });
 }
+
+function cartadd(componentNo) {
+
+    $.ajax({
+        url: "",
+        data: {},
+        success: function(result) {
+            if (confirm("장바구니에 담았습니다. 장바구니 페이지로 이동하시겠습니까? ") == true) {
+                location.href = "/cart/productcart";
+            }
+        }
+    });
+}
